abb967849dff04435343164ff2bbcd66a0d2433a
Aydar2007 <128406771+Aydar2007@users.noreply.github.com>
2023-04-27 01:17:36 +0600

    HOMEWORK №8

---
+++ Homework_8.py
@@ -0,0 +1,46 @@
+a=[5,6,4,7,3,8,2,1,9,22,56,89,78]
+b=[]
+while True:
+    abc = min (a)
+    b.append(abc)
+    a.remove(abc)
+    if a == []:
+        break
+print(b)
+Value=3
+right = b.index(max(b))
+left = b.index(min(b))
+OK="on"
+while OK=="on":
+    if left<=right:
+        center = (left+right) // 2
+        if Value == b[center]:
+            print(f"число {b[center]}")
+            OK="OFF"
+        else:
+            if Value>b[center]:
+                left = center+1
+            else:
+                right = center-1
+    else:
+        OK="OFF"
+        print("Нет искаемого числа")
+
+# while L < R:
+#     if L<R:
+#         i=()
+#         if x>i:
+#             L=i+1
+#         else:
+#             R=i
+#             Pos=R
+#             ResultOk = True
+#     if ResultOk == True:
+#         print("Элемент найден")
+#         print(Pos)
+#     else:
+#         print("Элемент не найден")
+    
+
+
+
